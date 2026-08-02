523eff96d79e18506587805571a05394dc4bee20
bossbuwi <paradoxhorizondevs@gmail.com>
2026-08-02 18:07:37 +0800

    0.8.6-alpha

--- src/main/java/com/paradoxdevs/dollar/DollarApplication.java
+++ src/main/java/com/paradoxdevs/dollar/DollarApplication.java
@@ -11,14 +11,15 @@ public class DollarApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DollarApplication.class);
 
-		if (isPostgresAvailable()) {
-			System.out.println(">>> PostgreSQL detected on 54322. Using Postgres profile.");
-			app.setAdditionalProfiles("postgres");
-		} else {
-			System.out.println(">>> PostgreSQL NOT detected. Falling back to H2 profile.");
-			app.setAdditionalProfiles("h2");
-		}
+//		if (isPostgresAvailable()) {
+//			System.out.println(">>> PostgreSQL detected on 54322. Using Postgres profile.");
+//			app.setAdditionalProfiles("postgres");
+//		} else {
+//			System.out.println(">>> PostgreSQL NOT detected. Falling back to H2 profile.");
+//			app.setAdditionalProfiles("h2");
+//		}
 
+		app.setAdditionalProfiles("postgres");
 		app.run(args);
 	}
 
